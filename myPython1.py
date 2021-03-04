e6ab22cc3545738a2ccfc48903ff242cf96ba41b
NissimElbaz <lbaz8970@gmail.com>
2021-03-04 21:18:26 +0200

    commit 3 21:18

--- myPython1.py
+++ myPython1.py
@@ -11,3 +11,10 @@ cls()
 name = input("what is your name?")
 print(f"welcome to you, {name}")
 print(f"{name.title()}, your name has {len(name)}")
+
+for index, letter in enumerate(name):
+    print(f"{index+1}: {letter}")
+    if letter == "s":
+        print("wow wow")
+    else:
+        print("🤴🤴🤴")
