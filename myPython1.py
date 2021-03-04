99fc26f3b14d9aa76b05400a74ebe3768058b2c7
NissimElbaz <lbaz8970@gmail.com>
2021-03-04 20:28:33 +0200

    second commit 20:28

--- myPython1.py
+++ myPython1.py
@@ -9,4 +9,5 @@ def cls(): return os.system('cls')
 cls()
 
 name = input("what is your name?")
-print(f"welcome to you {name}")
+print(f"welcome to you, {name}")
+print(f"{name.title()}, your name has {len(name)}")
